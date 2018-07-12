76374a301279ca437bc6d5d87ca067e76f2ad109
ocean998 <40862700+ocean998@users.noreply.github.com>
2018-07-12 11:45:23 +0800

    new get_dest_dir

--- get_new_file.py
+++ get_new_file.py
@@ -1,13 +1,15 @@
 import os
 import re
+import shutil
 
-#path = 'C:/Users/Administrator/Desktop'
+# path = 'C:/Users/Administrator/Desktop'
 path = r'D:\__各地市督办核查单'
 
 os.chdir(r'D:\__各地市督办核查单')
 
 dq = {'杭州': '01', '嘉兴': '02', '绍兴': '03', '金华': '04', '宁波': '05',
       '台州': '06', '丽水': '07', '湖州': '08', '衢州': '09', '温州': '10', '舟山': '11'}
+
 
 # 列出在目录 ×××__各地市督办核查单\2018×××\ ××\..\ 下面的文件名为 工作核查单***.xxx 的文件
 # 列出在在目录 ×××__各地市督办核查单\2018×××\ ××\..\ 下面的文件名为 工作督办单***.xxx   的文件
@@ -22,16 +24,17 @@ class Doc_name():
         self.lb = xflb
 
         for root, dirs, files in list_dirs:
-            print(root)
-            print(dirs)
-            print(files)
-            print('_________________')
+            # print(root)
+            # print(dirs)
+            # print(files)
+            # print('_________________')
             for f in files:
                 fn = os.path.join(root, f)
                 if fn.find(r'__各地市督办核查单\2018') > 1 and fn.find(r'\工作核查单') > 1:
                     if os.path.basename(fn).find(
                             '核查') > 1 and os.path.dirname(fn).find('原件') > 1:
-                        self.hcd.append(os.path.basename(fn))
+                        self.hcd.append(fn)
+                        print(fn)
 
                 if fn.find(r'__各地市督办核查单\2018') > 1 and fn.find(r'\工作督办单') > 1:
                     if os.path.basename(fn).find(
@@ -68,27 +71,34 @@ class where_storing():
             '温州': '10',
             '舟山': '11'}
         list_dirs = os.walk(self.basedir)
-        # os.chdir(path)
-        # os.fchdir(fd)
-        # os.getcwd()
+
         self.storing_dir = []
         for root, dirs, files in list_dirs:
             if os.path.basename(root) in dq:
-                self.storing_dir.append(root+'='+dq[os.path.basename(root)])
-                # print(root)
-                # continue
-            # print(root)
-            # print(dirs)
-            # print(files)
+                self.storing_dir.append(
+                    root + '=' + dq[os.path.basename(root)])
+
+    def get_dest_dir(self):
+        print('\n**********要生成的核查单目录***********\n')
+        p = re.compile('=[0-9]{2}')
+
+        self.dest_dirs = []
         for x in self.storing_dir:
-            print(x)
+            pp = p.search(x)
+            self.dest_dirs.append(x[0:pp.span()[0]])
+            print(x[0:pp.span()[0]])
+            pp = p.search(x).group(0)
+            pp = pp[1:3]
+            if
+        return self.dest_dirs
+
 
 # 根据文件名列表 计算新的文件名
 
 
-def get_new_fname(old_fn):
+def get_new_fname(old_fn, new_path):
     # p 定义要匹配的正则表达式条件
-    p = re.compile(r'([0-9]+).([0-9]+).([0-9]+)')
+    p = re.compile(r'([0-9]{4}).([0-9]{2}).([0-9]{3})')
     old_s = old_fn
     # 取出第三个匹配的，为文件序号
     xh = p.search(old_s).group(3)
@@ -101,25 +111,28 @@ def get_new_fname(old_fn):
     else:
         new_s = str(int(xh) + 1)
     new_s = old_s.replace(xh, new_s)
+    new_s = str(new_path) + '\\' + os.path.basename(new_s)
     # print(old_s + '\nrename to  \n' + new_s)
     return new_s
 
 
-# test = Doc_name(path, '核查')
-# x = test.get_name_list()
-# y = []
-# print(type(x))
-# for n in x:
-#     # print('核查单 : %s' %n)
-#     if n.find('-08-') > 1:
-#         y.append(n)
-#
-# y.sort(reverse=True)
-# print('*******************')
-# print(y[0])
-#
-# fn = get_new_fname(y[0])
-# print(fn)
+test = Doc_name(path, '核查')
+x = test.get_name_list()
+y = []
+for n in x:
+    # print('核查单 : %s' %n)
+    if n.find('-08-') > 1:
+        y.append(n)
 
+y.sort(reverse=True)
+print('*******************')
+print(y[0])
+
+fn = get_new_fname(y[0], 'D:\__各地市督办核查单\_新核查')
+print(fn)
+shutil.copyfile(y[0], fn)
 
 newdir = where_storing(r'D:\__各地市督办核查单\_新核查', '核查单')
+new_dir = newdir.get_dest_dir()
+for d in new_dir:
+    print(d)
