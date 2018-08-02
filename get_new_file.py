a0b179e0df8e5c7c44a1587a83d3104b17704297
ocean998 <40862700+ocean998@users.noreply.github.com>
2018-08-02 15:02:48 +0800

    end

--- get_new_file.py
+++ get_new_file.py
@@ -1,58 +1,47 @@
 import os
 import re
+import shutil
 
-#path = 'C:/Users/Administrator/Desktop'
+# path = 'C:/Users/Administrator/Desktop'
 path = r'D:\__各地市督办核查单'
+path_new = 'D:\__各地市督办核查单\_新核查'
 
 os.chdir(r'D:\__各地市督办核查单')
 
 dq = {'杭州': '01', '嘉兴': '02', '绍兴': '03', '金华': '04', '宁波': '05',
       '台州': '06', '丽水': '07', '湖州': '08', '衢州': '09', '温州': '10', '舟山': '11'}
 
+
 # 列出在目录 ×××__各地市督办核查单\2018×××\ ××\..\ 下面的文件名为 工作核查单***.xxx 的文件
 # 列出在在目录 ×××__各地市督办核查单\2018×××\ ××\..\ 下面的文件名为 工作督办单***.xxx   的文件
 
 
 class Doc_name():
-    def __init__(self, path, xflb):
+    def __init__(self, path,new_path, xflb):
         self.pt = path
+        self.basedir = new_path
         list_dirs = os.walk(self.pt)
         self.dbd = []
         self.hcd = []
-        self.lb = xflb
+        if str(xflb).find('核查') > 0:
+            self.lb = 'hcd'
+        else:
+            self.lb = 'dbc'
 
+        # 初始化核查单和督办单的历史文件列表
         for root, dirs, files in list_dirs:
-            print(root)
-            print(dirs)
-            print(files)
-            print('_________________')
             for f in files:
                 fn = os.path.join(root, f)
                 if fn.find(r'__各地市督办核查单\2018') > 1 and fn.find(r'\工作核查单') > 1:
                     if os.path.basename(fn).find(
                             '核查') > 1 and os.path.dirname(fn).find('原件') > 1:
-                        self.hcd.append(os.path.basename(fn))
+                        self.hcd.append(fn)
+                        # print(fn)
 
                 if fn.find(r'__各地市督办核查单\2018') > 1 and fn.find(r'\工作督办单') > 1:
                     if os.path.basename(fn).find(
                             '督办') > 1 and os.path.dirname(fn).find('原件') > 1:
                         self.dbd.append(os.path.basename(fn))
-
-    def get_name_list(self):
-        if str(self.lb).find('核查') > 0:
-            return self.dbd
-        else:
-            return self.hcd
-
-
-class where_storing():
-    # base：要生成文件的基础目录，db_hc：督办单，核查单
-    def __init__(self, base, db_hc):
-        self.basedir = base
-        if str(db_hc).find('') >= 0:
-            self.lb = 'dbd'
-        else:
-            self.lb = 'hcd'
 
         # 在基础目录中查找 哪些地市需要生成督办 或核查 单
         dq = {
@@ -66,29 +55,63 @@ class where_storing():
             '湖州': '08',
             '衢州': '09',
             '温州': '10',
-            '舟山': '11'}
-        list_dirs = os.walk(self.basedir)
-        # os.chdir(path)
-        # os.fchdir(fd)
-        # os.getcwd()
+            '舟山': '11' }
+
+        # 要生成哪些单位的的督办、核查单，其后跟单位编码 dq
+        list_dirs = os.walk( self.basedir )
         self.storing_dir = []
         for root, dirs, files in list_dirs:
-            if os.path.basename(root) in dq:
-                self.storing_dir.append(root+'='+dq[os.path.basename(root)])
-                # print(root)
-                # continue
-            # print(root)
-            # print(dirs)
-            # print(files)
+            if os.path.basename( root ) in dq:
+                self.storing_dir.append(
+                    root + '=' + dq[os.path.basename( root )] )
+
+    # 获取之前的核查、督办列表
+    def get_name_list(self):
+        if self.lb  =='hcd':
+            return self.dbd
+        else:
+            return self.hcd
+
+    # 要生成的核查单目录，单位后没有加 督办核查单原件 子目录的，添加该子目录
+    def get_dest_dir(self):
+        # print('\n**********要生成的核查单目录***********\n')
+        p = re.compile('=[0-9]{2}')
+
+        self.dest_dirs = []
         for x in self.storing_dir:
-            print(x)
+            pp = p.search(x)
+
+            fn = x[0:pp.span()[0]] + r'\督办核查单原件'
+            if not os.path.exists(fn):
+                os.mkdir(fn)
+
+            pp = p.search(x).group(0)
+            pp = pp[1:3]
+            fn = fn + ':' + pp
+            self.dest_dirs.append(fn)
+        return self.dest_dirs
+
+    def copy_doc(self, dest_dir):
+        dir = dest_dir + r'\督办核查单原件'
+        if os.path.exists(dir):
+            print(dir + '\t ok')
+
+    # 获取新的准备下发的核查单作为模板
+    def GetSource(self):
+        for lists in os.listdir(self.basedir):
+            path = os.path.join(self.basedir, lists)
+
+            if os.path.isfile(path):
+                return path
+        else:
+            return 'not exists file!'
+# end of class Doc_name():
+
 
 # 根据文件名列表 计算新的文件名
-
-
-def get_new_fname(old_fn):
+def get_new_fname(old_fn, new_path):
     # p 定义要匹配的正则表达式条件
-    p = re.compile(r'([0-9]+).([0-9]+).([0-9]+)')
+    p = re.compile(r'([0-9]{4}).([0-9]{2}).([0-9]{3})')
     old_s = old_fn
     # 取出第三个匹配的，为文件序号
     xh = p.search(old_s).group(3)
@@ -101,25 +124,45 @@ def get_new_fname(old_fn):
     else:
         new_s = str(int(xh) + 1)
     new_s = old_s.replace(xh, new_s)
+    new_s = str(new_path) + '\\' + os.path.basename(new_s)
     # print(old_s + '\nrename to  \n' + new_s)
     return new_s
 
 
-# test = Doc_name(path, '核查')
-# x = test.get_name_list()
-# y = []
-# print(type(x))
-# for n in x:
-#     # print('核查单 : %s' %n)
-#     if n.find('-08-') > 1:
-#         y.append(n)
-#
-# y.sort(reverse=True)
-# print('*******************')
-# print(y[0])
-#
-# fn = get_new_fname(y[0])
-# print(fn)
 
 
-newdir = where_storing(r'D:\__各地市督办核查单\_新核查', '核查单')
+
+test = Doc_name(path,r'D:\__各地市督办核查单\_新核查', '核查')
+x = test.get_name_list()
+
+
+# 最后下发的核查的核查单名称 重新命名，序号加一
+
+fs = test.GetSource()
+# shutil.copyfile(y[0], fn)
+
+
+print("###############")
+new_dir = test.get_dest_dir()
+
+for d in new_dir:
+    p = re.compile(':[0-9]{2}')
+    pp = p.search(d)
+
+    fn = d[0:pp.span()[0]]
+
+    pp = p.search(d).group(0)
+    pp = '-' + pp[1:3] + '-'
+    # print( pp )
+    y = []
+    # x = Doc_name.get_name_list()
+    for n in x:
+        # print('核查单 : %s' %n)
+        if n.find(pp) > 1:
+            y.append(n)
+            # 将找到的某地市核查单名称反向排序，第一条即为最后下发的核查的核查单名称
+            y.sort(reverse=True)
+            print('old:'+y[0])
+            fn = get_new_fname(y[0], 'D:\__各地市督办核查单\_新核查\湖州\督办核查单原件')
+            print(fn)
+            # shutil.copyfile( y[0], fn )
